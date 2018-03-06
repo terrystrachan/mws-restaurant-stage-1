155e70fd94db0666211bd2078327706e25a81ecf
terrystrachan <terry@trs-net.co.uk>
2018-03-06 23:03:32 +0000

    feat: added basic service worker files

---
+++ sw.js
@@ -0,0 +1,7 @@
+// self.addEventListener('fetch',function(event) {
+//     event.respondWith(
+//         new Response('a <b>string</b> here', {
+//             headers: {'Content-Type':'text/html'}
+//         })
+//     );
+// })
